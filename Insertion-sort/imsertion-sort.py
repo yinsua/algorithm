cec44d732a0fb0304658202e6f2b14a2bee542a5
yinsua <ysh.001@qq.com>
2015-08-11 19:30:19 +0800

    基本版本

--- Insertion-sort/imsertion-sort.py
+++ Insertion-sort/imsertion-sort.py
@@ -1,18 +1,21 @@
 #/usr/bin/python
 #coding:utf-8
 
+#插入排序
+#2015-08-11
+
 import os
 import sys
 import time
 
-def insertionSort(x):
-	for y in range(len(x)):
-		key = x[y]
-		a = y-1
-		while a >= 0 and x[a] > key:
-			x[a+1] = x[a]
-			a = a-1
-		x[a+1] = key
+def insertionSort(List):
+	for loop in range(len(List)):		
+		after = List[loop]
+		front = loop-1
+		while front >= 0 and List[front] > after:
+			List[front+1] = List[front]
+			front = front-1
+		List[front+1] = after
 		#print x
 
 x = [32,465,46,41,30,8,98,564]
