5ff79749ff29a2a8fe0f5d5f89460577b2a6f1c5
yinsua <ysh.001@qq.com>
2015-08-11 19:08:28 +0800

    ok

--- Insertion-sort/imsertion-sort.py
+++ Insertion-sort/imsertion-sort.py
@@ -5,4 +5,17 @@ import os
 import sys
 import time
 
-print 'test'
+def insertionSort(x):
+	for y in range(len(x)):
+		key = x[y]
+		a = y-1
+		while a >= 0 and x[a] > key:
+			x[a+1] = x[a]
+			a = a-1
+		x[a+1] = key
+		#print x
+
+x = [32,465,46,41,30,8,98,564]
+print "raw :\t" + `x`
+insertionSort(x)
+print "after :\t" + `x`	
